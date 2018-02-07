e8d1fc486da4caa7059c8d17769acba1ff06eab1
AIMS\rfisher <r.fisher@aims.gov.au>
2018-02-07 09:26:43 +0800

    moved ElicitN function code to body of questionair script to resolve issues with finding the right tt toplevel widget.

--- ee_component1.tcltk.r
+++ ee_component1.tcltk.r
@@ -12,7 +12,7 @@
 #(http://www.gnu.org/licenses/) for more details.
 
 survey.stage="E"
-tt <- tktoplevel()
+tt <<- tktoplevel()
 tkwm.title(tt,"Expert elicitation survey - Part E, Parameter elicitation.")
 tkgrid(tklabel(tt,text="    ",font=fontBlank)) # Blank line
 
@@ -42,7 +42,128 @@ tkgrid(tklabel(tt,text=
 We will start by thinking how big or how small you think this number could be.",font=fontText))
 tkgrid(tklabel(tt,text="    ",font=fontBlank)) # Blank line
 
-elicitN(default.vars.component1)
+#elicitN(default.vars.component1)
+def.vars=default.vars.component1
+done.var <<- tclVar(0)
+plot.var <<- tclVar(0)
+change.var <<- tclVar(0)
+
+Units <<- tclVar(def.vars["Units"])
+entry.Units <-tkentry(tt,width="30",font=fontText,textvariable=Units)
+tkgrid(tklabel(tt,text=
+"Please type the units of magnitude you would like to use for your estimates (1's,10's,100's,1000's,etc)",font=fontSubText),entry.Units)
+entry.Units <-tkentry(tt,width="30",font=fontText,textvariable=Units)
+
+Smallest <<- tclVar(def.vars["Smallest"])
+entry.Smallest <-tkentry(tt,width="30",font=fontText,textvariable=Smallest)
+tkgrid(tklabel(tt,text=
+"In the units supplied above, what is the smallest this number could be?", font=fontText),entry.Smallest)
+tkgrid(tklabel(tt,text="(You would be really surprised if the number was smaller than this.)",font=fontSubText))
+
+Largest <<- tclVar(def.vars["Largest"])
+entry.Largest <-tkentry(tt,width="30",font=fontText,textvariable=Largest)
+tkgrid(tklabel(tt,text=
+"What is the largest this number could be?",font=fontText),entry.Largest)
+tkgrid(tklabel(tt,text="(You would be really surprised if the number was larger than this.)",font=fontSubText))
+tkgrid(tklabel(tt,text="    ",font=fontBlank)) # Blank line
+
+# Lower and Upper --------------------------------------------------------------
+tkgrid(tklabel(tt,text=
+"This means you are nearly 100% sure that the number is in this range. We need to bring these bounds in a bit,
+so you won't be 100% sure the number falls inside these bounds.",font=fontSubText))
+tkgrid(tklabel(tt,text="    ",font=fontBlank)) # Blank line
+
+Lower <<- tclVar(def.vars["Lower"])
+entry.Lower <-tkentry(tt,width="30",font=fontText,textvariable=Lower)
+tkgrid(tklabel(tt,text=
+"What is a more realistic lower bound?",font=fontText),entry.Lower)
+
+Upper <<- tclVar(def.vars["Upper"])
+entry.Upper <-tkentry(tt,width="30",font=fontText,textvariable=Upper)
+tkgrid(tklabel(tt,text=
+"What is a more realistic upper bound?",font=fontText),entry.Upper)
+
+tkgrid(tklabel(tt,text=
+"Just so we are all on the same page, how much rounding are you doing here?
+Eg Does [2,000] mean the same as 1,750-2,250, or 1,900-2,100 or 1,999-2,001?",font=fontSubText))
+#[Change the number to be the same as the lower bound = answer to Q3.]
+
+# ------------------------------------------------------------------------------
+Sureness <<- tclVar(def.vars["Sureness"])
+entry.Sureness <-tkentry(tt,width="10",font=fontText,textvariable=Sureness)
+tkgrid(tklabel(tt,text=
+"How sure are you that the real number lies in these bounds? (%)",font=fontText),entry.Sureness)
+
+Best <<- tclVar(def.vars["Best"])
+entry.Best<-tkentry(tt,width="30",font=fontText,textvariable=Best)
+tkgrid(tklabel(tt,text=
+"What is your best guess, the most likely value?",font=fontText),entry.Best)
+tkgrid(tklabel(tt,text="(the one where you think you are most likely to be right with this estimate compared with any other)",font=fontSubText))
+tkgrid(tklabel(tt,text="    ",font=fontBlank)) # Blank line
+
+tkgrid(tklabel(tt,text=
+"To help you get your numbers right, we can now give some feed back on the implications of what you've just said:
+how sure you are, and the corresponding lower and upper bounds. We can feedback some bounds for a different level
+of certainty.", font=fontText))
+tkgrid(tklabel(tt,text=
+"Eg 50% chance (even odds) that the true number is in the interval, 67% chance (2-1 odds), 80% chance (4-1 odds), 95% chance (19-1 odds).",font=fontSubText))
+
+New.alpha <<- tclVar(def.vars["New.alpha"])
+entry.New.alpha <-tkentry(tt,width="30",font=fontText,textvariable=New.alpha)
+tkgrid(tklabel(tt,text=
+"Which would you prefer?",font=fontText),entry.New.alpha)
+
+# Examine plot:
+#Then the bounds corresponding to this level of certainty would be...
+#Do you think these bounds are too narrow or too wide or just right?
+
+# Create "calculate and plot", "finished" and "back" buttons
+
+#Finished.but <- tkbutton(tt,text="  Next  ",font=fontText,
+#                  command=function() {
+#                  #Output the information as a list
+#                  temp.out<<-c(
+#                  "Units"=tclvalue(Units),
+#                  "Smallest"=tclvalue(Smallest),
+#                  "Largest"=tclvalue(Largest),
+#                  "Lower"=tclvalue(Lower),
+#                  "Upper"=tclvalue(Upper),
+#                  "Sureness"=tclvalue(Sureness),
+#                  "Best"=tclvalue(Best),
+#                  "New.alpha"=tclvalue(New.alpha))
+#
+#               if(tclvalue(plot.var)<2){
+#                    tkmessageBox(message = "You must get feedback before you may proceed")}else{
+#                  tclvalue(done.var)<<-2
+#                  more.components<<-1}
+#                   })
+tkbind(tt,"<Destroy>",function() tclvalue(done.var)<<-3)
+
+Plot.but <- tkbutton(tt,text="  Get feedback  ",font=fontText,
+                     command=plot_routine)
+
+Skip.but <- tkbutton(tt,text="  Finish  ",font=fontText,
+                  command=function() {
+                  #Output the information as a list
+                  temp.out<<-c(
+                  "Units"=tclvalue(Units),
+                  "Smallest"=tclvalue(Smallest),
+                  "Largest"=tclvalue(Largest),
+                  "Lower"=tclvalue(Lower),
+                  "Upper"=tclvalue(Upper),
+                  "Sureness"=tclvalue(Sureness),
+                  "Best"=tclvalue(Best),
+                  "New.alpha"=tclvalue(New.alpha))
+
+               if(tclvalue(plot.var)<2){
+                    tkmessageBox(message = "You must get feedback before you may proceed")}else{
+                  tclvalue(done.var)<<-3
+                  more.components<<-0}
+                   })
+tkgrid(Plot.but,Skip.but)  #Finished.but,
+tkwait.variable(done.var)
+try(dev.off(),silent=TRUE)
+
 default.vars.component1[names(temp.out)]=temp.out
 expert.data<<-tclvalue(Data)
 default.vars.component1["Data"]<-expert.data
