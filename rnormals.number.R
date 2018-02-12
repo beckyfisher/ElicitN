a3f425e7b247b73fbffc95df18149d66aa425f91
beckyfisher <r.fisher@aims.gov.au>
2018-02-12 15:31:02 +0800

    Modified function to version 2 which properly handles negative values.

--- rnormals.number.R
+++ rnormals.number.R
@@ -12,15 +12,32 @@ rnormals.number <- function(n, mu, sig, which.dist,use.lower,use.upper) {
 #use.upper=as.numeric(data.i[e,"NallMar.u"])
 
 #temp <-(((MostUpper - MostLower)* (rbeta(n, alpha, beta) - 1) )+ MostUpper)
+#n=1000000, mu=data.raw$mu[i] , sig=data.raw$sig [i],which.dist= data.raw$use.lower[i],data.raw$use.upper[i]
+
   if(which.dist=="normal"){
     temp=rnorm(n, mu, sig)
-    }  
+    temp=temp[which(temp>=0)]
+    temp=sample(temp,n,replace=T)
+   # while(length(temp)<n){
+   #     tt=rnorm(1, mu, sig)
+   #     if(tt>=0){temp=c(temp,tt)}}
+   }
   if(which.dist=="LN"){
     temp=rlnorm(n, mu, sig)
-    }   
+    temp=temp[which(temp>=0)]
+    temp=sample(temp,n,replace=T)
+    #while(length(temp)<n){
+    #    tt=rlnorm(n, mu, sig)
+    #    if(tt>=0){temp=c(temp,tt)}}
+    }
   if(which.dist=="LNleftskew"){
     temp=use.lower - rlnorm(n, mu, sig) + use.upper
+    temp=temp[which(temp>=0)]
+    temp=sample(temp,n,replace=T)
+    #while(length(temp)<n){
+    #    tt=use.lower - rlnorm(1, mu, sig) + use.upper
+    #    if(tt>=0){temp=c(temp,tt)}}
     }
- temp[which(temp<0)]=0   
+
 return(temp)
 }
